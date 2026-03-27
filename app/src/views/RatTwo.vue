0b5e86df89a892ad7fcba3e258b9b6ae80eda2f0
Kortnee Rong <kortneer@nycstudents.net>
2026-03-27 12:31:45 -0400

    jkbnm

---
+++ app/src/views/RatTwo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script setup>
+    const myChart = new Chart(ctx, {...});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
